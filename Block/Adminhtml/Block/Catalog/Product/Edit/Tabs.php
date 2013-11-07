6104ad849101cee8739e8bd6e9897f7e729b0ecc
Cosmo Mao <mao@excecbc.com>
2013-11-08 02:17:58 +0800

    remove product tags

---
+++ Block/Adminhtml/Block/Catalog/Product/Edit/Tabs.php
@@ -0,0 +1,8 @@
+<?php
+class CosmoCommerce_ThemeFas01_Block_Adminhtml_Catalog_Product_Edit_Tabs extends Mage_Adminhtml_Block_Catalog_Product_Edit_Tabs
+{
+    protected function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
