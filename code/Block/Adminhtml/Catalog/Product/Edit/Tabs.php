44e5ff0f72af00d9a700e262cfbdc6bfb2e6252e
root <root@AY131108180833355af4Z.(none)>
2013-11-23 01:10:29 +0800

    fix for products tabs

--- code/Block/Adminhtml/Catalog/Product/Edit/Tabs.php
+++ code/Block/Adminhtml/Catalog/Product/Edit/Tabs.php
@@ -4,6 +4,22 @@ class CosmoCommerce_ThemeFas01_Block_Adminhtml_Catalog_Product_Edit_Tabs extends
     protected function _prepareLayout()
     {
         parent::_prepareLayout();
+        
+        foreach($this->_tabs as $tab){
+            if($tab->title== Mage::helper('adminhtml')->__('Prices')  ){
+                $this->removeTab($tab->tab_id);
+            }
+            if($tab->title== Mage::helper('adminhtml')->__('Design')  ){
+                $this->removeTab($tab->tab_id);
+            }
+            if($tab->title== Mage::helper('adminhtml')->__('Recurring Profile')  ){
+                $this->removeTab($tab->tab_id);
+            }
+            if($tab->title== Mage::helper('adminhtml')->__('Gift Options')  ){
+                $this->removeTab($tab->tab_id);
+            }
+        }
+        
         $this->removeTab('tags');
         $this->removeTab('customers_tags');
         return ;
